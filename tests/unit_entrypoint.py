e4b19baeb76cf1652ea542920da0196c029d03d7
Janrey Licas <self.codexlink@gmail.com>
2021-06-22 00:40:28 +0800

    Unit Tests (removed) + Several Changes (README.md)

--- tests/unit_entrypoint.py
+++ tests/unit_entrypoint.py
@@ -17,17 +17,4 @@ limitations under the License.
 from subprocess import Popen
 from typing import Final
 
-from yaml import load, FullLoader
-from .constraints import *
-# Popen("CLS", shell=True).commmunicate(). Crashes, idk why.
-
-docs = []
-# Fetch Input Action Keys
-with open('../action.yml') as file:
-    doc = load(file, Loader=FullLoader)
-    docs = list(doc["inputs"])
-
-
-
-for i in docs:
-    print(docs)
+""" To be done later."""
