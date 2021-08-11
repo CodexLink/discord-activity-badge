f8a77660092be3710dac7767c65f5dfdd4f56ac4
Janrey Licas <self.codexlink@gmail.com>
2021-08-11 14:35:33 +0800

    Constant (left-out): Updated.

--- src/elements/constants.py
+++ src/elements/constants.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
 from datetime import datetime
 from enum import Enum, IntEnum, auto, unique
-from logging import DEBUG, ERROR, INFO, WARNING
+from logging import CRITICAL, DEBUG, ERROR, INFO, WARNING
 from time import strftime
 from typing import Any, Final, Optional, TypedDict
 from aiohttp import BasicAuth
